2fba584ecbc6df403bee1fd2a702576e2c5f438a
Miguel Reis Ribeiro de Almeida <miguel.reis@hotmail.com.br>
2025-08-07 16:56:19 -0300

    Add files via upload

---
+++ aula 7/script.js
@@ -0,0 +1,60 @@
+// let numero = prompt("Digite um número:");
+// if (numero >0) {
+//     console.log("O número é positivo");
+// } else {
+//     console.log("O número é negativo");
+// }
+
+
+// let idade = prompt("digite sua idade =")
+// if(idade>=16 ){
+//     console.log("vc pode votar")
+// }
+// else{console.log("vc nn pode votar")}
+
+
+// let numero = prompt("Digite um número inteiro:");
+// if (numero % 2 === 0) {
+//     console.log("O número é par");
+// } else {
+//     console.log("O número é impar");
+// }
+
+
+// let n1=prompt("digite o 1º numero")
+// let n2=prompt("digite o 2º numero")
+// let n3=prompt("digite o 3º numero")
+// if (n1==n2==n3==n1){
+//     console.log('é um triangulo equilátero')
+// }
+// if (n1!=n2!=n3!=n1){
+//  console.log('é um triangulo escaleno')}
+// else{
+//     console.log('é um triangulo isóceles')
+// }
+
+
+// let N=prompt("digite um numero para verifiacar se e um multiplo de 8 e 16")
+// if (N %8===0 && N %16==0){
+// console.log("O número é múltiplo de 8 e 16");}
+//  else {console.log("O número NÃO é múltiplo de 8 e 16");}
+
+
+// let N=prompt('digite um numero para verificar se é positivo e maior q 15')
+// if (N > 15) {
+//     console.log('Seu número é maior que 15 e positivo');
+// } else if (N > 0) {
+//     console.log('Seu número é menor ou igual a 15 e positivo');
+// } else {
+//     console.log('Seu número é negativo');
+// }
+
+
+// let N = prompt('Digite um número para verificar se ele é divisível por 9 ou 6');
+// if (N % 6 === 0 ) {
+//     console.log("O número é divisível por 9 ou 6");
+// } else {
+//     console.log("O número NÃO é divisível por 9 nem por 6");
+// }
+
+
